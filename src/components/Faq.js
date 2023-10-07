eb0606fb0ea3db7b497a4fc6addbe08ccc105e30
Nikhil <ns84993@gmail.com>
2023-10-07 14:00:11 +0530

    faq

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,36 +1,71 @@
-import Accordion from "react-bootstrap/Accordion";
+import { Chevron } from "./icon";
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+} from "react-headless-accordion";
+import { faqData } from "./index_a";
+import { useCallback, useMemo } from "react";
 
-function Faq() {
+const Faq = () => {
+  const faqDataSize = useMemo(() => {
+    return faqData.length;
+  }, []);
+
+  const generateHeaderClass = useCallback(
+    (open, position) => {
+      const background = open ? "bg-gray-300/30" : "bg-white";
+      const border =
+        position === faqDataSize ? (open ? "border-b" : "") : "border-b";
+      return `transition-[background] flex items-center justify-between w-full px-4 py-3 ${border} ${background}`;
+    },
+    [faqDataSize]
+  );
+
+  const generateBodyClass = useCallback(
+    (open, position) => {
+      return position === faqDataSize ? "" : open ? "border-b" : "";
+    },
+    [faqDataSize]
+  );
+
   return (
     <section>
-      <Accordion defaultActiveKey="0" flush>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
+      <Accordion
+        className="rounded-md overflow-hidden border"
+        alwaysOpen={true}
+      >
+        {faqData.map((item, index) => (
+          <AccordionItem key={index}>
+            {({ open }) => (
+              <>
+                <AccordionHeader
+                  className={generateHeaderClass(open, index + 1)}
+                >
+                  <span className="text-sm font-semibold text-gray-600">
+                    {item.title}
+                  </span>
+
+                  <Chevron
+                    className={`w-6 h-6 text-gray-500 transition duration-300 ${
+                      open ? "rotate-90" : ""
+                    }`}
+                  />
+                </AccordionHeader>
+
+                <AccordionBody className={generateBodyClass(open, index + 1)}>
+                  <div className="p-4 text-sm text-gray-500">
+                    {item.content}
+                  </div>
+                </AccordionBody>
+              </>
+            )}
+          </AccordionItem>
+        ))}
       </Accordion>
     </section>
   );
-}
+};
 
 export default Faq;
