1f792391dac26bb903c83a88baffeb75d8c00764
Nikhil <ns84993@gmail.com>
2023-10-07 14:24:51 +0530

    faq

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -7,6 +7,8 @@ import {
 } from "react-headless-accordion";
 import { faqData } from "./index_a";
 import { useCallback, useMemo } from "react";
+import "../css/faq.css";
+import d1 from "../assets/doodle1.png";
 
 const Faq = () => {
   const faqDataSize = useMemo(() => {
@@ -31,40 +33,46 @@ const Faq = () => {
   );
 
   return (
-    <section>
-      <Accordion
-        className="rounded-md overflow-hidden border"
-        alwaysOpen={true}
-      >
-        {faqData.map((item, index) => (
-          <AccordionItem key={index}>
-            {({ open }) => (
-              <>
-                <AccordionHeader
-                  className={generateHeaderClass(open, index + 1)}
-                >
-                  <span className="text-sm font-semibold text-gray-600">
-                    {item.title}
-                  </span>
+    <>
+      <div className="head">Frequently Asked Questions</div>
+      <section className="faq">
+        <Accordion
+          className="rounded-md overflow-hidden border accordion"
+          alwaysOpen={true}
+        >
+          {faqData.map((item, index) => (
+            <AccordionItem key={index}>
+              {({ open }) => (
+                <>
+                  <AccordionHeader
+                    className={generateHeaderClass(open, index + 1)}
+                  >
+                    <span className="text-sm font-semibold text-gray-600">
+                      {item.title}
+                    </span>
 
-                  <Chevron
-                    className={`w-6 h-6 text-gray-500 transition duration-300 ${
-                      open ? "rotate-90" : ""
-                    }`}
-                  />
-                </AccordionHeader>
+                    <Chevron
+                      className={`w-6 h-6 text-gray-500 transition duration-300 ${
+                        open ? "rotate-90" : ""
+                      }`}
+                    />
+                  </AccordionHeader>
 
-                <AccordionBody className={generateBodyClass(open, index + 1)}>
-                  <div className="p-4 text-sm text-gray-500">
-                    {item.content}
-                  </div>
-                </AccordionBody>
-              </>
-            )}
-          </AccordionItem>
-        ))}
-      </Accordion>
-    </section>
+                  <AccordionBody className={generateBodyClass(open, index + 1)}>
+                    <div className="p-4 text-sm text-gray-500">
+                      {item.content}
+                    </div>
+                  </AccordionBody>
+                </>
+              )}
+            </AccordionItem>
+          ))}
+        </Accordion>
+        <div className="lap-img">
+          <img src={d1} alt="" />
+        </div>
+      </section>
+    </>
   );
 };
 
